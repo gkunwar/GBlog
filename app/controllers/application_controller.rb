8ae6892a511d74806073649889c3f222b71b8c26
gkunwar <gkunwar09@gmail.com>
2014-12-25 01:19:48 +0545

    Redirect to the user sign_in path from sign_up path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_to_login
+
+  private
+  def redirect_to_login
+    if params[:controller] == "devise/registrations" && params[:action] == 'new'
+      redirect_to new_user_session_path
+    end
+  end
 end
